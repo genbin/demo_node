c369f80d30873a8ead136eb9fb4438801bfce6d4
liugenbin <liugenbin@weizoom.com>
2016-11-10 18:32:05 +0800

    增加数据库连接

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 var express = require('express')
-var app = express()
 
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+    host: 'mysql',
+    user: 'root',
+    password: 'weizoom', 
+    database: 'mysql'
+});
+conn.connect();
+console.log("we're connected mysql!");
+conn.end();
+
+var mongo = require('mongoose');
+mongo.connect('mongodb://mongo/local');
+var db = mongo.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+   console.log("we're connected mongo!");
+});
+
+var redis = require('redis');
+
+var app = express()
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
   res.send('\nwelcome to the node world')
